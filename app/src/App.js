1c7794aaace9e835ed889490ac82e0a93fd5b748
Aaron Williams <aaronrw4@yahoo.com>
2019-11-21 21:00:20 -0800

    added video

--- app/src/App.js
+++ app/src/App.js
@@ -5,7 +5,7 @@ import DogImg from './components/dogImg'
 function App() {
   return (
     <div className="App">
-      <h1>Random Dog Picture</h1>
+      <h1>Dogs!!!</h1>
       <DogImg/>
     </div>
   );
--- app/src/components/dogImg.js
+++ app/src/components/dogImg.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {getDogData} from '../actions';
+import ReactPlayer from 'react-player';
 
 const DogData = props => {
+    let string = props.dogImg;
+    let type = string.substr(string.length -3);
+    console.log(type)
+
     return (
         <div>
+            {console.log(props.dogImg)}
             <div className='button'>
                 <button onClick={() => {
                     props.getDogData();
@@ -13,7 +19,16 @@ const DogData = props => {
                 </button>
             </div>
             <div>
-                <img src={props.dogImg} className='dogImg'/>
+                {type === 'mp4' ? 
+                    <ReactPlayer 
+                        url={props.dogImg} 
+                        playing 
+                        loop 
+                        height='80vh'
+                        className='video'
+                    /> :
+                    <img src={props.dogImg} alt='' className='dogImg'/>
+                }
             </div>
         </div>
     )
